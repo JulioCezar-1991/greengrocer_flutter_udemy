1dc2863184bbf85bdc982136f3958688e7b121a1
juliocezar-1991 <juliocezar_1991@hotmail.com>
2022-08-23 18:07:39 -0300

    created screen home_tab_ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'src/auth/sign_in_screen.dart';
+import 'src/pages/auth/sign_in_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: const SignInScreen());
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.white.withAlpha(190),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SignInScreen(),
+    );
   }
 }
---
+++ lib/src/pages/home/components/item_tile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:greengrocer/src/config/custom_colors.dart';
+import 'package:greengrocer/src/model/item_model.dart';
+
+class ItemTile extends StatelessWidget {
+  const ItemTile({Key? key, required this.item}) : super(key: key);
+
+  final ItemModel item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 1,
+      shadowColor: Colors.grey.shade300,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Image.asset(item.imgUrl),
+            ),
+            Text(
+              item.itemName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Row(
+              children: [
+                Text(
+                  item.price.toString(),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: CustomColors.customSwatchColor),
+                ),
+                Text(
+                  '/${item.unit}',
+                  style: TextStyle(
+                      color: Colors.grey.shade500,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
